ff5768ce7ce04df91a66c0662d4a7feb55d156c2
Christine Doza <cdoza@leoevents.com>
2022-05-23 14:09:55 -0500

    articles

---
+++ components/ArticleList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ArticleItem from "./ArticleItem";
+import articleStyles from "../styles/Article.module.css";
+
+const ArticleList = ({ articles }) => {
+  return (
+    <div className={articleStyles.grid}>
+      {articles.map((article) => (
+        // eslint-disable-next-line react/jsx-key
+        <ArticleItem article={article} />
+      ))}
+    </div>
+  );
+};
+
+export default ArticleList;
